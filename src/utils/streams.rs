08da187309a6b56cc4e0616e8b4d5d0d5cc6d825
Matthew Jobin <matt.jobin@ucsf.edu>
2025-04-14 07:35:57 -0700

    Added stream_to_cmd for async streaming to and from external software.

--- src/utils/streams.rs
+++ src/utils/streams.rs
@@ -1,8 +1,11 @@
 // src/utils/stream.rs
 use std::fs::File;
+use std::io;
 use std::io::Write;
 use tokio::sync::mpsc;
 use tokio::sync::broadcast;
+use tokio::process::{Command, Child};
+use tokio::io::AsyncWriteExt;
 use tokio_stream::StreamExt;
 use tokio_stream::wrappers::BroadcastStream;
 use tokio_stream::wrappers::errors::BroadcastStreamRecvError;
@@ -173,3 +176,55 @@ pub async fn tee_fastx(
 }
 
 
+/// Asynchronously spawn an external process and feed it a stream as stdin.
+/// Capture stdout and return from function.
+///
+/// # Arguments
+///
+/// * `stream' - Receiver stream: tokio::mpsc
+/// * 'command' - command to shell out
+/// * 'args' = args for shelled out command
+///
+/// # Returns
+/// tokio::process::Command containing stdout and stderr
+pub async fn stream_to_cmd<T>(
+    mut stream: BroadcastStream<T>,
+    command: &str,
+    args: &[&str],
+) -> io::Result<Child>
+where
+    T: Clone + AsRef<[u8]> + Send + 'static,
+{
+    let mut child = Command::new(command)
+        .args(args)
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped()) // Optional
+        .stderr(std::process::Stdio::piped()) // Optional
+        .spawn()?;
+    
+    let mut stdin = child.stdin.take().ok_or_else(|| {
+        io::Error::new(io::ErrorKind::Other, "Failed to open stdin")
+    })?;
+    
+    tokio::spawn(async move {
+        while let Some(result) = stream.next().await {
+            match result {
+                Ok(data) => {
+                    if let Err(e) = stdin.write_all(data.as_ref()).await {
+                        eprintln!("Failed to write to stdin: {}", e);
+                        break;
+                    }
+                    if let Err(e) = stdin.write_all(b"\n").await {
+                        eprintln!("Failed to write newline: {}", e);
+                        break;
+                    }
+                }
+                Err(BroadcastStreamRecvError::Lagged(skipped)) => {
+                    eprintln!("Stream lagged, skipped {} items", skipped);
+                }
+            }
+        }
+    });
+
+    Ok(child)
+}
