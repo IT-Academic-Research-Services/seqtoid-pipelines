722456be3216a70babbed48012afc957190f490c
Matthew Jobin <matt.jobin@ucsf.edu>
2025-08-12 12:49:57 -0700

    commented old_run

--- src/main.rs
+++ src/main.rs
@@ -82,6 +82,11 @@ async fn main() -> Result<()> {
 }
 
 async fn consensus_genome_run(run_config: Arc<RunConfig>) -> Result<(), PipelineError> {
+
+    // consensus_genome::old_run(&run_config)
+    //     .await
+    //     .map_err(|e| PipelineError::Other(e.into()))
+
     consensus_genome::run(run_config).await
 }
 
