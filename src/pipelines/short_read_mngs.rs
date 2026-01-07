57bdd19509c01be35584fa76adedf5d7c935ea3a
Matthew Jobin <matt.jobin@ucsf.edu>
2026-01-07 07:19:53 -0800

    60

--- src/pipelines/short_read_mngs.rs
+++ src/pipelines/short_read_mngs.rs
@@ -2106,13 +2106,12 @@ async fn diamond_non_host_align(
 
     let diamond_options = HashMap::from([
         ("--mid-sensitive".to_string(), None),
-        ("--block-size".to_string(), Some("50".to_string())),
+        ("--block-size".to_string(), Some("60".to_string())),
         // ("--block-size".to_string(), Some(format!("{:.1}", optimal_block_size))),
         ("-f".to_string(), Some("6".to_string())),
         ("-o".to_string(), Some(temp_path.to_string_lossy().into_owned())),
         ("--tmpdir".to_string(), Some(diamond_tmpdir.to_string_lossy().into_owned())),
         ("--index-mode".to_string(), Some("1".to_string())),
-        ("--memory-limit".to_string(), Some("897".to_string())),
         ("--range-culling".to_string(), None),
         ("--verbose".to_string(), None),
     ]);
