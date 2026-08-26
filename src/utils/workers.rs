93ae8ed6ae4c659969230fbd273dbccc4b2c7f1d
Matthew Jobin <matt.jobin@ucsf.edu>
2026-08-26 05:58:49 -0700

    constants for interoperating with EC2 instances as laid out in project plan.

--- src/utils/workers.rs
+++ src/utils/workers.rs
@@ -8,3 +8,13 @@ use aws_sdk_ec2::{
 use serde::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
 use tokio::fs;
+
+
+pub const WORKER_ROLE_TAG: &str = "Role";
+pub const WORKER_ROLE_VALUE: &str = "seqtoid-nr-cpu-worker";
+pub const WORKER_BACKEND_TAG: &str = "Backend";
+pub const WORKER_BACKEND_VALUE: &str = "cpu";
+pub const WORKER_REFERENCE_SET_TAG: &str = "ReferenceSet";
+pub const WORKER_REFERENCE_SET_VALUE: &str = "phase2";
+pub const WORKER_ENVIRONMENT_TAG: &str = "Environment";
+pub const WORKER_ENVIRONMENT_VALUE: &str = "dev";
